9cfd262052706d4545ba9e39e4fd95a8e4f02be5
Yuri Kuznetsov <playeer2905@gmail.com>
2025-07-21 19:08:16 +0300

    WIP

--- app/(profile)/profile/page.tsx
+++ app/(profile)/profile/page.tsx
@@ -1,11 +1,6 @@
-import { CarListing } from '@/components/shared/car-listing'
 import { getUserSession } from '@/components/shared/constants/get-user-session'
 import { Container } from '@/components/shared/container'
-import { Filters } from '@/components/shared/filters'
-import { Header } from '@/components/shared/header'
 import { ProfileForm } from '@/components/shared/profile-form'
-import { Search } from '@/components/shared/search'
-import { findCars, GetSearchParams } from '@/lib/find-cars'
 import { prisma } from '@/prisma/prisma-client'
 import { redirect } from 'next/navigation'
 
@@ -24,13 +19,11 @@ export default async function Profile() {
 
     return (
         <>
-            <Header />
             <main className="w-full">
                 <Container>
                     <ProfileForm user={user} />
                 </Container>
             </main>
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
         </>
     )
 }
